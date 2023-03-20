f56ebf2dbd280a84eddf9b1506f34f0643c8c9a8
digitpepper <postmaster@digitpepper.com>
2023-03-20 13:55:41 +0800

    Initial commit

---
+++ src/Database.php
@@ -0,0 +1,160 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace DP;
+
+use PDO;
+use PDOStatement;
+use Exception;
+
+class Database
+{
+	/**
+	 * @var bool
+	 */
+	public static $is_constructed = false;
+
+	/** @var PDO $pdo */
+	public static $pdo = null;
+
+	/**
+	 * @var array<int, int|string>
+	 */
+	protected static $options = [];
+
+	/**
+	 * @var string
+	 */
+	protected static $host = null;
+
+	/**
+	 * @var string
+	 */
+	protected static $unix_socket = null;
+
+	/**
+	 * @var string
+	 */
+	protected static $name = null;
+
+	/**
+	 * @var string
+	 */
+	protected static $user = null;
+
+	/**
+	 * @var string
+	 */
+	protected static $password = null;
+
+	public static function construct(): void
+	{
+		if(self::$is_constructed){
+			return;
+		}
+		$options = [
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
+		];
+		if (\defined('\DB_SSL_CA')) {
+			$options[PDO::MYSQL_ATTR_SSL_CA] = \DB_SSL_CA;
+		}
+		if (\defined('\DB_PERSISTENT')) {
+			$options[PDO::ATTR_PERSISTENT] = \DB_PERSISTENT;
+		}
+		if (\defined('\DB_EMULATE_PREPARES')) {
+			$options[PDO::ATTR_EMULATE_PREPARES] = \DB_EMULATE_PREPARES;
+		}
+		$sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+		if (\defined('\DB_SQL_MODE')) {
+			$sql_mode = \DB_SQL_MODE;
+		}
+		$options[PDO::MYSQL_ATTR_INIT_COMMAND] .= ', sql_mode="' . $sql_mode . '"';
+		self::$options = $options;
+		self::$host = \defined('\DB_HOST') ? \DB_HOST : null;
+		self::$unix_socket = \defined('\DB_UNIX_SOCKET') ? \DB_UNIX_SOCKET : null;
+		self::$name = \DB_NAME;
+		self::$user = \DB_USER;
+		self::$password = \DB_PASSWORD;
+		self::connect();
+		self::$is_constructed = true;
+	}
+
+	public static function begin(): void
+	{
+		self::construct();
+		if (!self::$pdo->beginTransaction()) {
+			throw new Exception('PDO cannot begin transaction.');
+		}
+	}
+
+	public static function commit(): void
+	{
+		self::construct();
+		if (!self::$pdo->commit()) {
+			throw new Exception('PDO cannot commit transaction.');
+		}
+	}
+
+	public static function roll_back(): void
+	{
+		self::construct();
+		if (!self::$pdo->rollBack()) {
+			throw new Exception('PDO cannot roll back.');
+		}
+	}
+
+	/**
+	 * @param string $sql
+	 * @param array<string, string|int|null|bool> $data
+	 * @return PDOStatement
+	 * @throws Exception
+	 */
+	public static function prepare_bind_execute(string $sql, array $data = []): PDOStatement
+	{
+		self::construct();
+		$sth = self::$pdo->prepare($sql);
+		if ($data) {
+			foreach ($data as $key => $value) {
+				switch (\gettype($value)) {
+					case 'string':
+						$data_type = PDO::PARAM_STR;
+						break;
+					case 'integer':
+						$data_type = PDO::PARAM_INT;
+						break;
+					case 'NULL':
+						$data_type = PDO::PARAM_NULL;
+						break;
+					case 'boolean':
+						$data_type = PDO::PARAM_BOOL;
+						break;
+					default:
+						$data_type = PDO::PARAM_STR;
+				}
+				if (!$sth->bindValue($key, $value, $data_type)) {
+					throw new Exception('PDO statement cannot bind value.');
+				}
+			}
+		}
+		if (!$sth->execute()) {
+			throw new Exception('SQL statement cannot be executed.');
+		}
+		return $sth;
+	}
+
+	protected static function connect(): void
+	{
+		$dsn = 'mysql:';
+		$host = self::$host;
+		$unix_socket = self::$unix_socket;
+		if ($host) {
+			$dsn .= "host=$host";
+		} else if ($unix_socket) {
+			$dsn .= "unix_socket=$unix_socket";
+		}
+		self::$pdo = new PDO("$dsn;dbname=" . self::$name, self::$user, self::$password, self::$options);
+	}
+}
